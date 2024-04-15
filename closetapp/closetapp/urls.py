3d296ab3211eee89efeddc8a556e0bac5b34bf3c
Sean <121913095+jseanlee@users.noreply.github.com>
2024-04-15 15:54:38 -0400

    Delete closetapp directory
    need to use django rest framework instead of django itself

--- closetapp/closetapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from base.views import index
-
-urlpatterns = [
-    path('', index, name='index'),
-    path("admin/", admin.site.urls),
-]
--- closetapp/clothes/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Clothes(models.Model):
-    name = models.Charfield(max_length=50)
-    clothing_type = models.
-    
